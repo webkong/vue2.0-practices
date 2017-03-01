d4501fad2aa3e2c6324bf60c967cd69487ea1f1d
webkong <772930508@qq.com>
2017-03-01 16:18:19 +0800

    Build

--- src/components/seller/Seller.vue
+++ src/components/seller/Seller.vue
@@ -1,8 +1,34 @@
 <template>
-
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <div class="title"></div>
+        <star :size="36" :score=""></star>
+      </div>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
-	export default {};
+  import star from 'components/star/star';
+  const ERROR_OK = 0;
+	export default {
+		data () {
+			return {
+				seller:{}
+      };
+    },
+    created () {
+			this.$http.get('/api/seller').then((res) => {
+				res = res.body;
+				if (res.errno === ERROR_OK){
+					this.seller = res.data;
+        }
+      });
+    },
+		components:{
+			star: star
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 
